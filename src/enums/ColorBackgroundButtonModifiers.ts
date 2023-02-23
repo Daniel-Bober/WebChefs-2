3a7084ab7f82cef883741946e23feb8514dd1b81
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2023-02-23 19:30:49 +0100

    feat: add ColorBackgroundButton enum

---
+++ src/enums/ColorBackgroundButtonModifiers.ts
@@ -0,0 +1,11 @@
+export enum BackgroundColor {
+    yellow = "background-yellow",
+
+    blue= "background-blue"
+}
+
+export enum TextColor {
+    black = "text-black",
+
+    white = "text-white"
+}
