021121d328c3a4610f626e47b795262640cd6cde
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2023-02-23 19:31:54 +0100

    feat: add HeroSection comp to App, fix typo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-
+  <main>
+    <HeroSection></HeroSection>
+  </main>
 </template>
 
 <script setup lang="ts">
 
+import HeroSection from "@/components/fintechSections/HeroSection.vue";
 </script>
 
 <style scoped>
