bfa4a522be1f310b6ed5b3fe93c88a49f13add5d
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2023-02-24 11:09:41 +0100

    feat: add benefit comp

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-
+  <main>
+    <BenefitsSection></BenefitsSection>
+  </main>
 </template>
 
 <script setup lang="ts">
 
+import BenefitsSection from "@/components/fintechSections/BenefitsSection/BenefitsSection.vue";
 </script>
 
 <style scoped>
