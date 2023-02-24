ac913ed2fe466525d16e9df439d23b5564a12578
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2023-02-24 02:19:09 +0100

    feat: add HeadlineContentSection

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-
+  <main>
+    <HeadlineContentSection></HeadlineContentSection>
+  </main>
 </template>
 
 <script setup lang="ts">
+import HeadlineContentSection from "@/components/fintechSection/HeadlineContentSection.vue";
+
 
 </script>
 
